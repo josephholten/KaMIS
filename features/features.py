e1583e22639d7bf24c4cf2e5cb3f36b6414d4f4d
Joseph Holten <joseph@holten.com>
2021-03-30 11:44:10 +0200

    updated features

--- features/features.py
+++ features/features.py
@@ -29,6 +29,7 @@ def features(g: nx.Graph) -> np.array:
     greedy_chr_nmbr = len(set(greedy_coloring.values()))
 
     def f(v):
+        assert type(v) == type(next(g.__iter__())), "possibly there was passed a wrong type of node"
         """ calculates the feature vector """
         return np.array([
             # graph-theoretical features
@@ -49,12 +50,13 @@ def features(g: nx.Graph) -> np.array:
 
 def search_for_graphs(keyword_list, graph_folder="instances"):
     # find matching paths for keywords
-    matched_paths = [[path for path in os.listdir(graph_folder) if kw in path and path[-6:] == ".graph"] for kw in keyword_list]
-    for paths in matched_paths:
+    matched_graphs = [{'kw': kw, 'path': [path for path in os.listdir(graph_folder) if kw in path and path[-6:] == ".graph"]} for kw in keyword_list]
+    for graph in matched_graphs:
         # check if kw's are unique
-        assert len(paths) == 1
-
-    return [graph_folder + ("/" if graph_folder[-1] != "/" else "") + paths[0] for paths in matched_paths]
+        assert len(graph['path']) == 1, "kw's selecting graph are ambiguous"
+        graph['path'], = graph['path']   # assign graph['path'] to the first obj in graph['path']
+        graph['path'] = graph_folder + ("/" if graph_folder[-1] != "/" else "") + graph['path']
+    return matched_graphs 
 
 
 def parse_graph(graph_lines: list[str]) -> nx.Graph: 
@@ -84,19 +86,19 @@ def parse_graph(graph_lines: list[str]) -> nx.Graph:
 
     return graph
 
-def get_graphs_and_labels(graph_paths):
-    graphs = []    
-    for graph_path in graph_paths:
+def get_graphs_and_labels(graph_objs):  # graph_objs of type: list of dicts, each dict needs entry 'kw' and 'path'
+    for graph_obj in graph_objs:
+        graph_path = graph_obj['path']
         # calculate MIS if neccesary
         graph_mis_path = graph_path[:-6]+".MIS"
         subprocess.run(["sh", "features/calc_mis.sh", graph_path, graph_mis_path])
         # read graph and labels from resp. files
         with open(graph_path) as graph_file, open(graph_mis_path) as mis_file:
-            graphs.append({
+            graph_obj.update({
                 'graph': parse_graph(graph_file.readlines()),
                 'labels': np.array(list(map(int, mis_file.readlines())))   
             })
-    return graphs
+    return graph_objs
 
 training_graphs_paths = search_for_graphs([
         "jazz",
@@ -157,17 +159,25 @@ bst.load_model("simple.model")
 num_stages = 5
 q = 0.4   # confidence niveau
 
+print("testing on graphs", *(graph['kw'] for graph in test_graphs))
+track_removals = {graph['kw']: [] for graph in test_graphs} 
+
 for stage in range(1, num_stages+1):
     label_pred = bst.predict(dtest)
     np.savetxt(f"prediction_stage{stage}.pred", label_pred)
 
     for graph in test_graphs:
         removal = np.array(graph['graph'].nodes)[label_pred <= q]
-        print(len(removal))
-        print(removal)
         graph['graph'].remove_nodes_from(removal)
         graph['labels'] = graph['labels'][np.where(label_pred > q)[0]]  # remove labels of removed nodes in test graph
+        track_removals[graph['kw']].append(len(removal))
 
     dtest = get_dmatrix_from_graphs(test_graphs)
-    
-# print(test_graphs[0].number_of_nodes())
+
+for graph in test_graphs:
+    print("in graph", graph['kw'], "removed a total of", sum(track_removals[graph['kw']]), "nodes", "(", *track_removals[graph['kw']], ")")
+
+print("solution quality:")
+for graph in test_graphs:
+    print("number of edges:", len(graph['graph'].edges))
+    # print("edges:\n", graph['graph'].edges)
