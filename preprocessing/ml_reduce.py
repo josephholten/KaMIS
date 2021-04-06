ca74b465196d4e2338eee27608778ffa19afa448
Joseph Holten <joseph@holten.com>
2021-04-06 11:10:39 +0200

    renamed vars

--- preprocessing/ml_reduce.py
+++ preprocessing/ml_reduce.py
@@ -1,18 +1,16 @@
-import copy
 import numpy as np
 import xgboost as xgb
 
 from features import features
 from loading_utils import metis_format_to_nx, search_for_graphs, get_graphs_and_labels, get_dmatrix_from_graphs
 
-test_graphs_paths = search_for_graphs([
+graphs_paths = search_for_graphs([
         "netscience"
         #"astro-ph"
 ])
-test_graphs = get_graphs_and_labels(test_graphs_paths)
-original_test_graphs = copy.deepcopy(test_graphs)
+graphs = get_graphs_and_labels(graphs_paths)
 
-dtest = get_dmatrix_from_graphs(test_graphs)
+data = get_dmatrix_from_graphs(graphs)
 
 bst = xgb.Booster()
 bst.load_model("simple.model")
@@ -20,22 +18,22 @@ bst.load_model("simple.model")
 num_stages = 5
 q = 0.7   # confidence niveau
 
-print("testing on graphs", *(graph.graph['kw'] for graph in test_graphs))
-track_removals = {graph.graph['kw']: [] for graph in test_graphs} 
+print("reducing graphs", *(graph.graph['kw'] for graph in graphs))
+track_removals = {graph.graph['kw']: [] for graph in graphs} 
 
 for stage in range(1, num_stages+1):
-    label_pred = bst.predict(dtest)
+    label_pred = bst.predict(data)
     np.savetxt(f"prediction_stage{stage}.pred", label_pred)
 
-    for graph in test_graphs:
+    for graph in graphs:
         removal = np.array(graph.nodes)[label_pred <= q]
         graph.remove_nodes_from(removal)
         graph.graph['labels'] = graph.graph['labels'][np.where(label_pred > q)[0]]  # remove labels of removed nodes in test graph
         track_removals[graph.graph['kw']].append(len(removal))
 
-    dtest = get_dmatrix_from_graphs(test_graphs)
+    dtest = get_dmatrix_from_graphs(graphs)
 
-for graph in test_graphs:
+for graph in graphs:
     print("in graph", graph.graph['kw'], "removed a total of", sum(track_removals[graph.graph['kw']]), "nodes", "(", *track_removals[graph.graph['kw']], ")")
 
 # solution quality: (of course first reduce graphs with KaMIS) use ML to reduce graphs further, then calculate MIS and compare to MIS on original kernal
