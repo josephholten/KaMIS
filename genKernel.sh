338f3e06fc7955db339c4d77cab5dc8dfc5bebb9
Joseph Holten <joseph@holten.com>
2021-04-06 18:09:10 +0200

    renamed and split task into two files

---
+++ genKernel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BASEFOLDER = "/home/graph_collection/independentset_instances"
+# BASEFOLDER="instances"
+KERNELFOLDER="/home/jholten/kernels"
+
+for graph in `ls $BASEFOLDER/*.graph`; do
+    GRAPHNAME=`basename $graph`
+    GRAPHNAME=${GRAPHNAME%.*}
+    # echo $GRAPHNAME
+    echo "deploy/weighted_branch_reduce $BASEFOLDER/$graph --kernel=$KERNELFOLDER/$GRAPHNAME.kernel" 
+done
---
+++ genMIS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BASEFOLDER = "/home/graph_collection/independentset_instances"
+# BASEFOLDER="instances"
+MISFOLDER="/home/jholten/mis"
+
+for graph in `ls $BASEFOLDER/*.graph`; do
+    GRAPHNAME=`basename $graph`
+    GRAPHNAME=${GRAPHNAME%.*}
+    # echo $GRAPHNAME
+    echo "deploy/weighted_branch_reduce $BASEFOLDER/$graph --output=$MISFOLDER/$GRAPHNAME.mis" 
+done
