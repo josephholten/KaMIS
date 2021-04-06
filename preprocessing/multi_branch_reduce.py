27dac39ee6de81446145757c5db0be91bad8514e
Joseph Holten <joseph@holten.com>
2021-04-06 16:44:49 +0200

    writing commands to file for gnu parallel now

--- preprocessing/multi_branch_reduce.py
+++ preprocessing/multi_branch_reduce.py
@@ -50,7 +50,7 @@ if calc_kernel:
     if options[0][0] == "": # meaning kernel option was passed with no path
         kernel_folder = graph_folder
     else:
-        kernel_folder = os.path.abspath(*options[0]) if calc_kernel else ""
+        kernel_folder = os.path.abspath(*options[0]) if calc_kernel and not kernel_folder else ""
 else:
     kernel_folder = ""
 
@@ -58,7 +58,7 @@ if calc_mis:
     if options[1][0] == "": # meaning mis option was passed with no path
         mis_folder = graph_folder
     else:
-        mis_folder = os.path.abspath(*options[1]) if calc_mis else ""
+        mis_folder = os.path.abspath(*options[1]) if calc_mis and not mis_folder else ""
 else:
     mis_folder = ""
 
@@ -69,23 +69,24 @@ graph_paths = search_for_graphs(keyword_list, graph_folder=graph_folder)
 if not graph_paths:
     print("no graphs found.")
 
-for idx, graph_path in enumerate(graph_paths, start=1):
-    print(f"graph #{idx}/{len(graph_paths)}, path: {graph_path}")
-    graph_name = graph_path[len(graph_folder):-6] 
+with open("run_kamis_mis", "w") as mis_file, open("run_kamis_kernel", "w") as kernel_file:
+    for idx, graph_path in enumerate(graph_paths, start=1):
+        print(f"graph #{idx}/{len(graph_paths)}, path: {graph_path}")
+        graph_name = graph_path[len(graph_folder):-6] 
 
-    if calc_kernel:
-        kernel_path = kernel_folder + graph_name + ".kernel"
-        if os.path.isfile(kernel_path) and os.path.getsize(kernel_path) > 0:   # kernel file does exist and isn't empty
-            print("already calculated kernel")
-        else:
-            print("calculating kernel:")
-            subprocess.run(["deploy/weighted_branch_reduce", graph_path, "--kernel=" + kernel_path, "--time_limit=3600", "--weight_source=uniform"])
+        if calc_kernel:
+            kernel_path = kernel_folder + graph_name + ".kernel"
+            if os.path.isfile(kernel_path) and os.path.getsize(kernel_path) > 0:   # kernel file does exist and isn't empty
+                print("already calculated kernel")
+            else:
+                print("calculating kernel:")
+                kernel_file.write(f"deploy/weighted_branch_reduce {graph_path} --kernel={kernel_path} --time_limit=3600 --weight_source=uniform")
 
-    if calc_mis:
-        mis_path = mis_folder + graph_name + ".MIS"
-        if os.path.isfile(mis_path) and os.path.getsize(mis_path) > 0:
-            print("already calculated MIS")
-        else:
-            print("calculating MIS:")
-            subprocess.run(["deploy/weighted_branch_reduce", graph_path, "--output=" + mis_path, "--time_limit=3600", "--weight_source=uniform"])
+        if calc_mis:
+            mis_path = mis_folder + graph_name + ".MIS"
+            if os.path.isfile(mis_path) and os.path.getsize(mis_path) > 0:
+                print("already calculated MIS")
+            else:
+                print("calculating MIS:")
+                mis_file.write("deploy/weighted_branch_reduce graph_path --output={mis_path} --time_limit=3600 --weight_source=uniform")
 
--- test.py
+++
@@ -1,8 +0,0 @@
-response = input("please respond: Y/n")
-if response == "" or response.lower() == "y":
-    print("you responded with: y")
-elif response.lower() == "n":
-    print("you responded with: n")
-else:
-    print("invalid response")
-
