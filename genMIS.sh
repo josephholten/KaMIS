9b5b6a7405138b63a05628f2adec2207e2ebc17e
Joseph Holten <joseph@holten.com>
2021-04-06 18:40:43 +0200

    .

--- genMIS.sh
+++ genMIS.sh
@@ -8,5 +8,5 @@ for graph in `find $BASEFOLDER -name '*.graph'`; do
     GRAPHNAME=`basename $graph`
     GRAPHNAME=${GRAPHNAME%.*}
     # echo $GRAPHNAME
-    echo "deploy/weighted_branch_reduce $graph --weight_source=uniform --time_limit=3600.0 --output=$MISFOLDER/$GRAPHNAME.mis" 
+    echo "deploy/weighted_branch_reduce $graph --weight_source=uniform --output=$MISFOLDER/$GRAPHNAME.mis" 
 done
